2ef0fc95263f7bea8510ba323b3b1467d05fd209
abushahid81 <abushahidsardar@gmail.com>
2024-04-26 03:08:03 +0530

    js

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import logo from "../assets/mainlogo.png";
+import logo from "../assets/mainlogo-o.png";
 import "./Header.css";
 
 function Header() {
